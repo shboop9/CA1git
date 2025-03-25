7ab9d0a1fdcbe91e454700cce83f6dba7d3f760d
shboop9 <shashikunigiri1@gmail.com>
2025-03-25 11:39:46 +0530

    CIVIL Switch Part added on test4 branch

--- programs.c
+++ programs.c
@@ -6,15 +6,22 @@ int main(){
     scanf("%d", &action);
 
     switch(action){
-    case 1:
-        printf("You Chose CSE!");
-        break;
-    case 2:
-        printf("Your Chose AIML!");
-        break;
-    }
-    case 3:
-        printf("Your Chose ECE!");
-        break;
+        case 1:
+            printf("You Chose CSE!");
+            break;
+        case 2:
+            printf("Your Chose AIML!");
+            break;
+
+        case 3:
+            printf("Your Chose ECE!");
+            break;
+
+        case 4:
+            printf("Your Chose CIVIL!");
+            break;
+
+        default:
+            printf("Invalid Input!");
     }
 }

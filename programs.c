84f68fc45d0539e93dec7e7f4c50347b6e76b165
shboop9 <shashikunigiri1@gmail.com>
2025-03-25 12:15:14 +0530

    change 3

--- programs.c
+++ programs.c
@@ -7,21 +7,15 @@ int main(){
 
     switch(action){
         case 1:
-            //the user chooses CSE
             printf("You Chose CSE!\n");
-            printf("Your are a computer engineer!");
             break;
 
         case 2:
-            //the user chooses AIML
             printf("Your Chose AIML!\n");
-            printf("Your are an AI engineer!");
             break;
 
         case 3:
-            //the use chooses ECE
             printf("Your Chose ECE!\n");
-            printf("Your are an electronics engineer");
             break;
 
         case 4:
